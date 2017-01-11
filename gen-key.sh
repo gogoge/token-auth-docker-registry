9d30a23e5e5313d7b20f8d271766ec75df012d47
Webb Lu <tsung.ying.lu@gmail.com>
2017-01-11 16:41:02 +0800

    add condition detect certs not exist

--- gen-key.sh
+++ gen-key.sh
@@ -1,3 +1,7 @@
+if [ ! -d "certs" ]; then
+  echo "Create Certificate and private key"
+  mkdir certs
+fi
 openssl req -newkey rsa:4096 -nodes -sha256 -keyout certs/domain.key \
             -subj '/C=TW/ST=Taiwan/L=Taipei/O=Wistron/OU=SWPC/CN=gogoge/' \
             -x509 -days 365 -out certs/domain.crt
